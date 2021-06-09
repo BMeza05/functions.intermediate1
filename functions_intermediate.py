332664d7c6e1faf9d03f9d50f6467cc4985d8c0e
Bmeza05 <brianmeza952@gmail.com>
2021-06-09 14:16:53 -0600

    initial commit

---
+++ functions_intermediate.py
@@ -0,0 +1,30 @@
+x = [ [5,2,3], [15,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Bryant'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+def iterateDictionary(students):
+    print(students[0]['first_name'])
+    print(students[1]['first_name'])
+iterateDictionary(students)
+
+
+
+def iterateDictionary2(students):
+    print(students[0]['last_name'])
+    print(students[1]['last_name'])
+iterateDictionary2(students)
+
+
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Andres', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 30} ]
+
+print("Hello")
+
+def printInfo(sports_directory):
+    printInfo(sports_directory)
+print(sports_directory['basketball'])
+print(sports_directory['soccer'])
